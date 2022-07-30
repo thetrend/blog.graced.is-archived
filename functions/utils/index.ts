b56a07ff6ca1838751a4ab47e098ee5c8e908dae
Grace de la Mora <grace.g.delamora@gmail.com>
2022-07-29 22:47:30 -0400

    feat(devops-01): trying a different env variable
    feat(devops-01): trying a different env variable

--- functions/utils/index.ts
+++ functions/utils/index.ts
@@ -11,7 +11,7 @@ const urlHelper = (event: HandlerEvent) => {
 };
 
 const dbHelper = (isAuthenticated: boolean = true, needsAdminPrivileges: boolean = false) => {
-  const QUERY_DATABASE: string = process.env['NODE_ENV'] || 'development';
+  const QUERY_DATABASE: string = process.env['BRANCH'] || 'development';
   const client = new faunadb.Client({
     secret: isAuthenticated ?
       `${process.env.AUTH_SECRET}:${QUERY_DATABASE}` as string :
