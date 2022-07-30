9fa8be10bfc1fd75edbd29576adb0b7d17c19e62
Grace de la Mora <grace.g.delamora@gmail.com>
2022-07-29 23:38:50 -0400

    should work

--- functions/helloWorld.ts
+++ functions/helloWorld.ts
@@ -6,14 +6,9 @@ const handler: Handler = async (event, context) => {
   await client.query(
     q.CreateDatabase({ name: 'test' })
   ).then(res => console.log(res)).catch(err => console.log(err));
-  const env = process.env.CONTEXT === 'production' ?
-    process.env.CONTEXT :
-    process.env.BRANCH === 'staging' ?
-      process.env.BRANCH :
-      'development';
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: `bonjour ${env}` })
+    body: JSON.stringify({ message: `bonjour` })
   };
 };
 
