4efe481fe8d5343c3c678be45c64178ac5524f48
Grace de la Mora <grace.g.delamora@gmail.com>
2022-07-30 00:01:50 -0400

    trying context var

--- functions/helloWorld.ts
+++ functions/helloWorld.ts
@@ -2,13 +2,13 @@ import { Handler } from '@netlify/functions';
 import { dbHelper } from '~NETLIFY/utils';
 
 const handler: Handler = async (event, context) => {
-  const { client, q } = dbHelper(false, true);
-  await client.query(
-    q.CreateDatabase({ name: 'test' })
-  ).then(res => console.log(res)).catch(err => console.log(err));
+  // const { client, q } = dbHelper(false, true);
+  // await client.query(
+  //   q.CreateDatabase({ name: 'test' })
+  // ).then(res => console.log(res)).catch(err => console.log(err));
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: `bonjour` })
+    body: JSON.stringify({ message: `bonjour ${process.env.CONTEXT}` })
   };
 };
 
