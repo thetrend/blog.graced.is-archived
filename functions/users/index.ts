55206436936f002500a36dc8390f5c2275a30f26
Grace de la Mora <grace.g.delamora@gmail.com>
2022-07-30 21:27:54 -0400

    feat(signup-01): Create a new users API endpoint

---
+++ functions/users/index.ts
@@ -0,0 +1,25 @@
+import { HandlerEvent } from '@netlify/functions';
+
+import { genericError, urlHelper } from '../utils';
+
+const handler = async (event: HandlerEvent) => {
+  try {
+    const endpoint = urlHelper(event);
+
+    let response;
+
+    switch (endpoint) {
+      default:
+        // TODO: redirect this to /api/users/me which will show the authenticated user's profile
+        response = {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'hello world' })
+        }
+    }
+    return response;
+  } catch (error) {
+    return genericError();
+  }
+};
+
+export { handler };
